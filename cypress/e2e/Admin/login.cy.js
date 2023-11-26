d62b00c5f67fa0d79e4e5bb59b49fc9a94624ea1
Kritartha Kc <kritarthakc@Kritarthas-MacBook-Air.local>
2023-11-26 15:38:44 +0545

    first commit

---
+++ cypress/e2e/Admin/login.cy.js
@@ -0,0 +1,9 @@
+const login = (email = 'admin@mail.com', password = 'MnbvcxZ@#1') => {
+  cy.visit('https://nepalehr.com');
+  cy.get('#email').type(email);
+  cy.get('#password').type(password);
+  cy.get('.rounded-lg').click();
+  // Add assertions here if needed
+};
+
+export default login;
