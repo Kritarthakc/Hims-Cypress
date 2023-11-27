1593582d3e88d77613ad2b936d84af77f689a5f2
Kritartha Kc <kritarthakc@Kritarthas-MacBook-Air.local>
2023-11-27 17:46:23 +0545

    asd

--- cypress.config.js
+++ cypress.config.js
@@ -4,4 +4,12 @@ module.exports = {
       // implement node event listeners here
     },
   },
-};
+  
+},
+
+
+{
+  "baseUrl": "http://localhost:3000",
+  "integrationFolder": "cypress/integration",
+  "testFiles": "**/*.{js,jsx,ts,tsx,coffee,spec.js,spec.ts}"
+}
---
+++ cypress/integration/example_spec.js
@@ -0,0 +1,7 @@
+describe('My First Test', () => {
+    it('Visits the homepage', () => {
+      cy.visit('/');
+      cy.contains('Welcome to My App');
+    });
+  });
+  
